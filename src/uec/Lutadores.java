986c70505ec6bee81cdc9678c57795e732f3b540
caua <cauakika@gmail.com>
2024-02-15 18:37:30 -0300

    adição de novo package de estudo

---
+++ src/uec/Lutadores.java
@@ -0,0 +1,29 @@
+package uec;
+
+public class Lutadores {
+	
+	Lutador prettyBoy = new Lutador("Pretty Boy", "França", 31, 1.75f, 68.9f, "leve", 11, 2, 1);
+	Lutador putsscrip = new Lutador("Putscript" , "Brasil", 29, 1.68f, 57.8f, "leve", 14, 2, 3);
+	Lutador SnapShadow = new Lutador("SnapShadow", "EUA"  , 35, 1.65f, 80.9f, "médio", 12, 2, 1);
+	Lutador deadCode = new Lutador("Dead Code","Australia", 28, 1.93f, 81.6f, "médio", 13, 0, 2);
+	Lutador ufoCobol = new Lutador("Ufo Cobol" , "Brasil" , 37, 1.70f, 119.3f,"pesado", 5, 4, 3);
+	Lutador nerdaard = new Lutador("Nerdaard" , "EUA"     , 30, 1.81f, 105.7f,"pesado", 12, 2, 4);
+	
+	public static void main (String []args) {
+	
+	
+	
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
