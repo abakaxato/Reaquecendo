740bbfa3b8a47d4dac84d9e06ea92df3ac521845
caua <cauakika@gmail.com>
2024-03-21 20:16:03 -0300

    :technologist: atualização da classe de exceções

--- src/fac/Testes.java
+++ src/fac/Testes.java
@@ -1,17 +1,18 @@
 package fac;
 
 public class Testes {
-	public static int divisao( int dividendo, int divisor) {
-	 return dividendo/divisor;	
-	};
 	
 	public static void main (String[]args) { 
-	
-		try {int resultado = divisao(10, 5);
+		
+	int  quociente, dividendo = 10, divisor =0;
+		
+		try {
+			if (divisor == 0) {
+				throw new ArithmeticException ("o divisore é zero.");
+			}
 			
-		}catch(ArithmeticException e) {
+		}catch(Exception e) {
 			System.out.println("Erro: divisão por zero");
 		}
-			System.out.println();
 	}
 }
