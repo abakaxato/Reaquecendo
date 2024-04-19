3b901816ac138c8895c951889a132363c3b2274e
caua <cauakika@gmail.com>
2024-04-18 21:03:43 -0300

    :fire: ta facil estudar exceções não

--- src/facul/Principal.java
+++ src/facul/Principal.java
@@ -1,32 +1,31 @@
 package facul;
+import java.io.FileReader;
+import java.io.BufferedReader;
 import java.io.IOException;
 public class Principal {
     public static void main ( String args [ ] ){
-    
+    BufferedReader leitor = null;
+    try {
+    	//abrir arquivo
+    	leitor = new BufferedReader(new FileReader("entrada.txt"));
+    	//leitura do conteudo do arquivo
+    	String linha;
+    	while ((linha= leitor.readLine())!= null) {
+    		//processe cada linha do arquivo
+    		System.out.println(linha);
+    	}
+    }catch(IOException e) {
+    	//tratar uma exceção de IO
+    	System.out.println("Ocorreu um erro durante a leitura do arquivo: " + e.getMessage());
+    }finally {
+    	//fechamento do arquivo no bloco finally
     	try {
-    	abrirArquivo();
-    	}catch(AbrirArquivoExcecao e) {
-    	System.out.println("um erro ocorreu na tentativa de abrir o arquivo : " + e.getMessage());
-    	System.out.println("Causa raiz: " + e.getCause().getMessage());
-    	}    	
-    }
-    
-    public static void abrirArquivo() throws AbrirArquivoExcecao{
-		try {
-			//simulando uma excecao ao abrir o arquivo
-			throw new IOException("incapaz de abrir o arquivo");
-		}catch(IOException e) {
-			// Envolvendo a exceção capturada em uma excerção personalizada
-			throw new AbrirArquivoExcecao("Ocorreu um erro ao abrir o arquivo", e);
-		}
-    }
-}
-
-
-class AbrirArquivoExcecao extends Exception{
-    public AbrirArquivoExcecao(String mensagem, Throwable causa) {
-    	super(mensagem, causa);
+    		if (leitor != null) {
+    			leitor.close();
+    		}
+    	}catch(IOException e) {
+    		System.out.println("Ocorrreu um erro durante o fechamento do arquivo: " + e.getMessage());
     	}
     }
-
- 
+   }
+}
